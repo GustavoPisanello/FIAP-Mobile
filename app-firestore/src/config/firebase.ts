09333b5fe6f1e398703c0bb64f950fc925c7292a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-10 22:32:12 -0300

    firebase

---
+++ app-firestore/src/config/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYIq5gJy74LjfJxEa9uOAOXFPAf2eEws4",
+    authDomain: "espx-mobile-c3feb.firebaseapp.com",
+    projectId: "espx-mobile-c3feb",
+    storageBucket: "espx-mobile-c3feb.firebasestorage.app",
+    messagingSenderId: "971956170202",
+    appId: "1:971956170202:web:dc31525003bfbeb6a764e3",
+    measurementId: "G-RPZED3CBT6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default app;
